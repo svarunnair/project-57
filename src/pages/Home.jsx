793c159aac141287ee1fc479c3e3e370375845ee
Varun Nair <varunsnair1997@gmail.com>
2024-04-14 01:38:42 +0530

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div
+        style={{
+          width: '200px',
+          height: '200px',
+          border: '2px solid red',
+          animationName: 'myframes', // Specify the name of the keyframes
+          animationDuration: '2s', // Set animation duration
+          animationTimingFunction: 'ease-in-out', // Set animation timing function
+          animationIterationCount: 'infinite' // Make animation infinite
+        }}
+      >
+        Animated Box
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
